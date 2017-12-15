88bd7ed3b136f73805659c6755d0c1d73e8cdb21
Kingnez <kenuyx@gmail.com>
2017-12-15 18:56:23 +0800

    scan barcode

--- app/src/main/java/sh/diqi/circuseyes/MainActivity.java
+++ app/src/main/java/sh/diqi/circuseyes/MainActivity.java
@@ -4,91 +4,95 @@ import android.graphics.SurfaceTexture;
 import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.serenegiant.common.BaseActivity;
 import com.serenegiant.usb.CameraDialog;
 import com.serenegiant.usb.USBMonitor;
-import com.serenegiant.usb.UVCCamera;
 import com.serenegiant.usbcameracommon.UVCCameraHandlerMultiSurface;
 import com.serenegiant.widget.UVCCameraTextureView;
 
 import java.util.ArrayList;
+import java.util.Locale;
+
+import cn.bingoogolapple.qrcode.core.QRCodeView;
 
 /**
  * Created by zengjing on 2017/12/11.
  */
 
-public class MainActivity extends BaseActivity implements CameraDialog.CameraDialogParent {
+public class MainActivity extends BaseActivity implements CameraDialog.CameraDialogParent, QRCodeView.Delegate {
 
-    /**
-     * set 0 if you want to record movie using MediaSurfaceEncoder
-     * (writing frame data into Surface camera from MediaCodec
-     * by almost same way as USBCameratest2)
-     * set 1 if you want to record movie using MediaVideoEncoder
-     */
-    private static final int ENCODER_TYPE = 1;
-
-    /**
-     * preview mode
-     * if your camera does not support specific resolution and mode,
-     * {@link UVCCamera#setPreviewSize(int, int, int)} throw exception
-     * 0:YUYV, other:MJPEG
-     */
-    private static final int PREVIEW_MODE = 1;
-
-    private static final int PREVIEW_WIDTH = 640;
-    private static final int PREVIEW_HEIGHT = 480;
+    private static final int PREVIEW_WIDTH = 320;
+    private static final int PREVIEW_HEIGHT = 240;
 
     private USBMonitor usbMonitor;
 
-    private ArrayList<UVCCameraTextureView> cameraViews = new ArrayList<>();
-    private ArrayList<UVCCameraHandlerMultiSurface> cameraHandlers = new ArrayList<>();
+    private ArrayList<TextView> cameraTips;
+    private ArrayList<UVCCameraTextureView> cameraViews;
     private int[] surfaceIds;
+    private UVCCameraHandlerMultiSurface[] cameraHandlers;
+
+    private EditText barcodeText;
+    private ArrayList<TextView> deviceTexts;
+    private ArrayList<TextView> captureTexts;
+
     private int processingIndex = -1;
+
+    private QRCodeView mQRCodeView;
 
     private final USBMonitor.OnDeviceConnectListener onDeviceConnectListener = new USBMonitor.OnDeviceConnectListener() {
 
         @Override
         public void onAttach(UsbDevice usbDevice) {
             Toast.makeText(MainActivity.this,
-                    "USB_DEVICE_ATTACHED", Toast.LENGTH_SHORT).show();
+                    usbDevice.getDeviceName() + "已接上", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onDettach(UsbDevice usbDevice) {
             Toast.makeText(MainActivity.this,
-                    "USB_DEVICE_DETACHED", Toast.LENGTH_SHORT).show();
+                    usbDevice.getDeviceName() + "已脱离", Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        public void onConnect(UsbDevice usbDevice, USBMonitor.UsbControlBlock usbControlBlock, boolean b) {
+        public void onConnect(final UsbDevice usbDevice, USBMonitor.UsbControlBlock usbControlBlock, boolean b) {
             Toast.makeText(MainActivity.this,
-                    "USB_DEVICE_CONNECTED", Toast.LENGTH_SHORT).show();
+                    "正在连接：" + usbDevice.getDeviceName(), Toast.LENGTH_SHORT).show();
             if (processingIndex == -1) {
                 return;
             }
-            cameraHandlers.get(processingIndex).open(usbControlBlock);
+            cameraHandlers[processingIndex].open(usbControlBlock);
             startPreview(processingIndex);
-            processingIndex = -1;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    deviceTexts.get(processingIndex).setText(usbDevice.getDeviceName());
+                    captureTexts.get(processingIndex).setText("预览中");
+                    processingIndex = -1;
+                }
+            });
         }
 
         @Override
         public void onDisconnect(UsbDevice usbDevice, USBMonitor.UsbControlBlock usbControlBlock) {
             Toast.makeText(MainActivity.this,
-                    "USB_DEVICE_DISCONNECTED", Toast.LENGTH_SHORT).show();
+                    "断开连接：" + usbDevice.getDeviceName(), Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onCancel(UsbDevice usbDevice) {
             Toast.makeText(MainActivity.this,
-                    "USB_DEVICE_CANCELED", Toast.LENGTH_SHORT).show();
+                    "取消连接：" + usbDevice.getDeviceName(), Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -104,15 +108,33 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
                 }
             }
             if (processingIndex > -1) {
-                if (!cameraHandlers.get(processingIndex).isOpened()) {
+                if (!cameraHandlers[processingIndex].isOpened()) {
                     CameraDialog.showDialog(MainActivity.this);
                 } else {
                     stopPreview(processingIndex);
-                    processingIndex = -1;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            deviceTexts.get(processingIndex).setText("未连接");
+                            captureTexts.get(processingIndex).setText("不可用");
+                            processingIndex = -1;
+                        }
+                    });
                 }
             }
         }
     };
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        Toast.makeText(this, "条码识别成功", Toast.LENGTH_SHORT).show();
+        barcodeText.setText(result);
+    }
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        Toast.makeText(this, "打开相机出错", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -121,37 +143,81 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
 
         usbMonitor = new USBMonitor(this, onDeviceConnectListener);
 
-        LinearLayout container = findViewById(R.id.textureViewContainer);
-        int viewCount = container.getChildCount();
-        surfaceIds = new int[viewCount];
-        for (int i = 0; i < container.getChildCount(); i++) {
-            UVCCameraTextureView cameraView = (UVCCameraTextureView) container.getChildAt(i);
-            cameraView.setAspectRatio(PREVIEW_WIDTH / (double) PREVIEW_HEIGHT);
-            cameraView.setOnClickListener(onViewClickListener);
-            cameraViews.add(cameraView);
-            cameraHandlers.add(UVCCameraHandlerMultiSurface.createHandler(this, cameraView,
-                    ENCODER_TYPE, PREVIEW_WIDTH, PREVIEW_HEIGHT, PREVIEW_MODE));
-            surfaceIds[i] = 0;
-        }
+        barcodeText = findViewById(R.id.barcode);
+
+        deviceTexts = getDeviceTexts();
+        captureTexts = getCaptureTexts();
+
+        cameraTips = getCameraTips();
+        cameraViews = getCameraViews();
+        surfaceIds = new int[cameraViews.size()];
+        cameraHandlers = new UVCCameraHandlerMultiSurface[cameraViews.size()];
+
+        final TextView bandwidthFactorValue = findViewById(R.id.bandwidthFactorValue);
+        final SeekBar bandwidthFactorSlider = findViewById(R.id.bandwidthFactorSlider);
+        bandwidthFactorSlider.incrementProgressBy(5);
+        bandwidthFactorSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                progress /= 5;
+                progress *= 5;
+                bandwidthFactorValue.setText(String.format(Locale.CHINESE, "0.%d", progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final Button bandwidthButton = findViewById(R.id.bandwidthConfirm);
+        bandwidthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float factor = Float.parseFloat(bandwidthFactorValue.getText().toString());
+                for (int i = 0; i < cameraViews.size(); i++) {
+                    final UVCCameraTextureView cameraView = cameraViews.get(i);
+                    cameraView.setAspectRatio(PREVIEW_WIDTH / (double) PREVIEW_HEIGHT);
+                    cameraView.setOnClickListener(onViewClickListener);
+                    cameraTips.get(i).setText(R.string.choose);
+                    final UVCCameraHandlerMultiSurface cameraHandler =
+                            UVCCameraHandlerMultiSurface.createHandler(MainActivity.this, cameraView,
+                                    PREVIEW_WIDTH, PREVIEW_HEIGHT, factor);
+                    cameraHandlers[i] = cameraHandler;
+                    surfaceIds[i] = 0;
+                }
+                bandwidthButton.setEnabled(false);
+            }
+        });
 
         Button captureButton = findViewById(R.id.buttonCapture);
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for (UVCCameraHandlerMultiSurface handler : cameraHandlers) {
-                    if (handler.isOpened() && checkPermissionWriteExternalStorage()) {
+                for (int i = 0; i < cameraHandlers.length; i++) {
+                    UVCCameraHandlerMultiSurface handler = cameraHandlers[i];
+                    if (handler != null && handler.isOpened() && checkPermissionWriteExternalStorage()) {
                         handler.captureStill();
+                        captureTexts.get(i).setText("存照中");
                     }
                 }
             }
         });
+
+        mQRCodeView = findViewById(R.id.zxingview);
+        mQRCodeView.setDelegate(this);
     }
 
     private void stopPreview(int index) {
         if (index == -1) {
             return;
         }
-        UVCCameraHandlerMultiSurface processingHandler = cameraHandlers.get(index);
+        UVCCameraHandlerMultiSurface processingHandler = cameraHandlers[index];
         if (surfaceIds[index] != 0) {
             processingHandler.removeSurface(surfaceIds[index]);
             surfaceIds[index] = 0;
@@ -160,7 +226,7 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
     }
 
     private void startPreview(final int index) {
-        cameraHandlers.get(index).startPreview();
+        cameraHandlers[index].startPreview();
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -170,7 +236,7 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
                     if (texture != null) {
                         Surface surface = new Surface(texture);
                         int surfaceId = surface.hashCode();
-                        cameraHandlers.get(index).addSurface(surfaceId, surface, false);
+                        cameraHandlers[index].addSurface(surfaceId, surface, false);
                         surfaceIds[index] = surfaceId;
                     }
                 } catch (final Exception e) {
@@ -180,26 +246,77 @@ public class MainActivity extends BaseActivity implements CameraDialog.CameraDia
         });
     }
 
+    private ArrayList<TextView> getCameraTips() {
+        ArrayList<TextView> views = new ArrayList<>();
+        RelativeLayout container = findViewById(R.id.cameraTips);
+        for (int i = 0; i < container.getChildCount(); i++) {
+            views.add((TextView) container.getChildAt(i));
+        }
+        return views;
+    }
+
+    private ArrayList<UVCCameraTextureView> getCameraViews() {
+        ArrayList<UVCCameraTextureView> views = new ArrayList<>();
+        RelativeLayout container = findViewById(R.id.textureViewContainer);
+        for (int i = 0; i < container.getChildCount(); i++) {
+            views.add((UVCCameraTextureView) container.getChildAt(i));
+        }
+        return views;
+    }
+
+    private ArrayList<TextView> getDeviceTexts() {
+        ArrayList<TextView> textViews = new ArrayList<>();
+        LinearLayout devices1 = findViewById(R.id.devices1);
+        for (int i = 0; i < devices1.getChildCount(); i++) {
+            textViews.add((TextView) devices1.getChildAt(i));
+        }
+        LinearLayout devices2 = findViewById(R.id.devices2);
+        for (int i = 0; i < devices2.getChildCount(); i++) {
+            textViews.add((TextView) devices2.getChildAt(i));
+        }
+        return textViews;
+    }
+
+    private ArrayList<TextView> getCaptureTexts() {
+        ArrayList<TextView> textViews = new ArrayList<>();
+        LinearLayout capture1 = findViewById(R.id.capture1);
+        for (int i = 0; i < capture1.getChildCount(); i++) {
+            textViews.add((TextView) capture1.getChildAt(i));
+        }
+        LinearLayout capture2 = findViewById(R.id.capture2);
+        for (int i = 0; i < capture2.getChildCount(); i++) {
+            textViews.add((TextView) capture2.getChildAt(i));
+        }
+        return textViews;
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
         usbMonitor.register();
+        mQRCodeView.startCamera();
+        mQRCodeView.startSpotAndShowRect();
     }
 
     @Override
     protected void onStop() {
+        mQRCodeView.stopSpotAndHiddenRect();
+        mQRCodeView.stopCamera();
         usbMonitor.unregister();
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
+        mQRCodeView.onDestroy();
         usbMonitor.destroy();
-        for (int i = 0; i < cameraHandlers.size(); i++) {
-            cameraHandlers.get(i).release();
-            cameraHandlers.set(i, null);
+        for (int i = 0; i < cameraHandlers.length; i++) {
+            if (cameraHandlers[i] != null) {
+                cameraHandlers[i].release();
+                cameraHandlers[i] = null;
+            }
         }
-        cameraHandlers.clear();
+        cameraHandlers = null;
         for (int i = 0; i < cameraViews.size(); i++) {
             cameraViews.set(i, null);
         }
